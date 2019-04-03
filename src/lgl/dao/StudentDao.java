aede18870cc30bb8bfd3d106114dc1e809543f4a
lgl1148571181 <1148571181@qq.com>
2019-04-03 15:45:33 +0800

    刘国梁提交cookie

--- src/lgl/dao/StudentDao.java
+++ src/lgl/dao/StudentDao.java
@@ -6,11 +6,47 @@ import entity.Student;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 //学生管理
 public class StudentDao extends BaseDao {
+
+
+
+    //学生登录
+    public Student login(int stuno,String pwd){
+        //连接数据库
+        Connection conn=super.getConn();
+        //登录学生信息
+        Student student=new Student();
+
+        try {
+            PreparedStatement ps=conn.prepareStatement("SELECT * FROM `student` WHERE studentno=? AND LoginPwd=? ");
+            ps.setInt(1,stuno);
+            ps.setString(2,pwd);
+
+            ResultSet rs= ps.executeQuery();
+
+            if(rs.next()){
+                //登录成功
+                student.setStudentNo(rs.getInt("studentno"));
+                student.setStudentName(rs.getString("StudentName"));
+                student.setSex(rs.getInt("sex"));
+                return student;
+            }else{
+                return null;//登录失败
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+
 
 
     public List<Student> queryStu(){
---
+++ src/lgl/servlet/LoginServlet.java
@@ -0,0 +1,54 @@
+package lgl.servlet;
+
+import entity.Student;
+import lgl.dao.StudentDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        StudentDao dao=new StudentDao();
+        //从表单获取账号密码
+        Integer username=Integer.parseInt(req.getParameter("username"));
+        String password=req.getParameter("password");
+
+
+        //登录方法
+        Student student= dao.login(username,password);
+
+        if(student!=null){
+            //登录成功以后判断用户是否记住账号
+            String ck=req.getParameter("rem");
+            if("1".equals(ck)){
+                //Cookie cookie=new Cookie("user",URLEncoder.encode(username,"UTF-8"));
+                //使用cookie记住账号
+                Cookie cookie=new Cookie("userName",username+"");
+                //设置cookie生命周期
+                cookie.setMaxAge(1*60);
+                resp.addCookie(cookie);
+            }
+
+            resp.sendRedirect("index.jsp");
+        }else{
+            resp.sendRedirect("login.jsp");
+        }
+
+
+
+    }
+}
